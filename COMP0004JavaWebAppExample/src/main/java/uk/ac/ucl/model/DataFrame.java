d9fe569b89c68ddc8fe5e92f8af18f41afcfe77e
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-03-09 11:04:58 +0000

    Changed to HashMap implementation

--- COMP0004JavaWebAppExample/src/main/java/uk/ac/ucl/model/DataFrame.java
+++ COMP0004JavaWebAppExample/src/main/java/uk/ac/ucl/model/DataFrame.java
@@ -1,51 +1,38 @@
 package uk.ac.ucl.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataFrame {
-    private ArrayList<Column> columns = new ArrayList<>();
-    private ArrayList<String> columnNames = new ArrayList<>();
+    private Map<String, Column> columns = new HashMap();
 
     public void addColumn(String columnName) {
-        columns.add(new Column(columnName));
-        columnNames.add(columnName);
+        columns.put(columnName, new Column(columnName));
     }
 
     public ArrayList<String> getColumnNames() {
-        return new ArrayList<>(columnNames);
+        return new ArrayList<>(columns.keySet());
     }
 
     public int getRowCount() {
+        if (columns.isEmpty()) {
+            return -1;
+        }
         // All columns should have the same number of rows
-        return columns.getFirst().getSize();
+        String randomKey = columns.keySet().iterator().next();
+        return columns.get(randomKey).getSize();
     }
 
     public String getValue(String columnName, int row) {
-        for (Column column: columns) {
-            if (column.getName().equals(columnName)) {
-                return column.getRowValue(row);
-            }
-        }
-        return "";
+        return columns.get(columnName).getRowValue(row);
     }
 
-    public boolean putValue(String columnName, int row, String value) {
-        for (Column column: columns) {
-            if (column.getName().equals(columnName)) {
-                column.setRowValue(row, value);
-                return true;
-            }
-        }
-        return false;
+    public void putValue(String columnName, int row, String value) {
+        columns.get(columnName).setRowValue(row, value);
     }
 
-    public boolean addValue(String columnName, String value) {
-        for (Column column: columns) {
-            if (column.getName().equals(columnName)) {
-                column.addRowValue(value);
-                return true;
-            }
-        }
-        return false;
+    public void addValue(String columnName, String value) {
+        columns.get(columnName).addRowValue(value);
     }
 }
